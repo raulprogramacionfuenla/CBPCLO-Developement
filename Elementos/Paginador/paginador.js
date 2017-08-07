ebc618c08d8286a2a90daa517a03230deb337901
Álvaro Peris <alvaroperis_06@hotmail.com>
2017-08-07 20:44:34 +0200

    Update paginador.js

--- Elementos/Paginador/paginador.js
+++ Elementos/Paginador/paginador.js
@@ -179,6 +179,122 @@
         	dataType: 'text',
             success : function (contenido) {
                 //Cargamos la nueva información información en el DOM
+                $(this.canvas).html(contenido);
+            	}
+			});//End AJAX
+		},//End refrescador
+		
+		//Pasa un json de entrada a un string quitando el último bracket y añadiendo una coma.
+		prepareIn: function(jso){
+			var stringform = JSON.stringify(jso);
+			return stringform.slice(0, -1) + ',';
+		},
+		
+		//Añade los campos de stack y paso a la información base proporcionada
+		appendSP: function(stack, paso){
+			var tmpStr = this.stringMsg;
+			tmpStr += '"stack": ' + stack + ', "paso": ' + paso + '}';
+			return tmpStr;
+		}
+
+	}//End paginador
+
+				//Transmitimos la información al servidor
+				this.ajaxCom();
+
+				if(this.debug) this.DebugInfo();
+			}//End if		
+		},
+
+		inicio: function (){
+			this.pagActual = 1;
+			this.stackC = 0;
+			this.printIndex();
+			this.state = 'init';
+
+			//Transmitimos la información al servidor
+			this.ajaxCom();
+			if(this.debug) this.DebugInfo();
+		},
+		
+		final: function (){
+			this.pagActual = this.numPags;
+			this.stackC = this.numElementos - this.paso;
+			this.printIndex();
+			this.state = 'final';
+
+			//Transmitimos la información al servidor
+			this.ajaxCom();
+			if(this.debug) this.DebugInfo();
+		},
+
+
+		//Controla las peticiones al servidor
+		componMensaje: function(status){
+			switch (status){
+				case 'init':	
+					return this.appendSP("0",this.paso);
+				break;
+			
+				case 'final':
+					var st = this.numElementos - this.paso;
+					return this.appendSP('"'+st.toString()+'"','"'+this.paso.toString()+'"');
+				break;
+
+				case 'pass':
+					if(this.stackC <= (this.numElementos - this.paso)){
+						var sc = this.stackC+ 1;
+						return this.appendSP((sc).toString(),this.paso.toString())
+					}
+				break;	
+			}//End switch
+
+
+		},
+
+		//-----> "Private" functions
+		printIndex: function(){
+			$('.rightElement b:nth-child(1)').html(this.pagActual);
+			$('.rightElement b:nth-child(2)').html(this.numPags);			
+		},
+
+		RoundNumElement: function(num){
+			 return Math.ceil(num / this.paso)*this.paso;
+		},
+
+		DebugInfo: function (){
+			console.log(":::::::::::::::::::::::::::::::::::::");
+			console.log("-> Página pagActual: " + this.pagActual);
+			console.log("-> Numero de pags: " + this.numPags);
+			console.log("---------------------------------");
+			console.log("-> Status: " + this.state);
+			console.log("-> Stack Counter: " + this.stackC);
+			console.log("-> Msg JSON Transmisión a backend: ");
+			console.log(this.componMensaje(this.state));
+			console.log(":::::::::::::::::::::::::::::::::::::\n\n");
+		},
+
+		objToString: function (obj) {
+    		var str = '';
+		    for (var p in obj) {
+		        if (obj.hasOwnProperty(p)) {
+		            str += '\t' + p + ':' + obj[p] + '\n' ;
+		        }
+		    }
+
+		    if(str != '') return str;
+		    else return 'No se transmite nada';
+		},
+
+		//Obtiene información de la base de datos.
+		//DirectoryPath representa la página donde se gestiona la petición
+		ajaxCom: function(elemento){
+			$.ajax({
+            type: "GET",
+        	url: this.directorio+'/'+this.componMensaje(this.state),
+        	dataType: 'text',
+            success : function (contenido) {
+                //Cargamos la nueva información información en el DOM
                 $('tbody').html(contenido);
             	}
 			});//End AJAX
