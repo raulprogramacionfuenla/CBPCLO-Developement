6b8d9dce4e793ad99ab143c3aff806c229324737
Álvaro Peris <alvaroperis_06@hotmail.com>
2017-11-22 22:14:09 +0100

    Alters de base de datos subidos en el dia 22

--- database/Alters16Nov2017.sql
+++ database/Alters16Nov2017.sql
@@ -1,31 +1,12 @@
-/*DAVID*/
-ALTER TABLE `fr_global_informe` CHANGE `id_frGlobal` `id_frGlobal`  INT NOT NULL AUTO_INCREMENT;
-ALTER TABLE `seguim_incomp_detall` CHANGE `id_seg_inc_det` `id_seg_inc_det` INT NOT NULL AUTO_INCREMENT;
-ALTER TABLE `seguim_incomp_resum` CHANGE `id_seg_inc_res` `id_seg_inc_res` INT NOT NULL AUTO_INCREMENT;
-
-/*ALVARO - Adaptación de campo a multiidioma*/
-ALTER TABLE `control` CHANGE `clase_control` `clase_control_CA` INT NOT NULL AUTO_INCREMENT;
-ALTER TABLE `control` ADD COLUMN `clase_control_ES` VARCHAR(30) NOT NULL AFTER `clase_control_CA`;
-
-/*SONIA*/
-ALTER TABLE `excepcio` CHANGE `id_excepcio`  `id_excepcio` INT NOT NULL AUTO_INCREMENT;
-CREATE INDEX ComponentControl ON `excepcio` (`componente`, `id_control` ASC);
-
-/*FRAN*/
-ALTER TABLE aplicacions_obsoletes ADD COLUMN  modulo VARCHAR(16) after aplicacio;
-
-ALTER TABLE `resultat_x_component_des` CHANGE `id_resultat` `id_resultat` INT(11) NOT NULL;
-ALTER TABLE `resultat_x_component_des_historic` CHANGE `id_resultat` `id_resultat` INT(11) NOT NULL;
-ALTER TABLE `resultat_x_component_pro` CHANGE `id_resultat` `id_resultat` INT(11) NOT NULL;
-ALTER TABLE `resultat_x_component_pro_historic` CHANGE `id_resultat` `id_resultat` INT(11) NOT NULL;
-
-/*MODS*/
-ALTER TABLE `germanar` CHANGE `id_germanar` `id_germanar` INT(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `regles_bloqueig` CHANGE `id_regla` `id_regla` INT(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `control` CHANGE `id_control` `id_control` INT(11) NOT NULL;
-
-
-delimiter |
+CREATE TABLE IF NOT EXISTS `excepcio_codi_harcode`(
+  `id_codi` INT NOT NULL AUTO_INCREMENT,
+  `tipus` VARCHAR(20) NOT NULL,
+  `excepcio` VARCHAR(100) NOT NULL,
+  `component` VARCHAR(8) NOT NULL,
+  `descripcio` VARCHAR(200),
+   PRIMARY KEY (`id_codi`),
+   UNIQUE INDEX `id_codi` (`id_codi` ASC))
+  ENGINE = InnoDB;|
 CREATE TRIGGER calculo_id_des_hist BEFORE INSERT ON resultat_x_component_des_historic
    FOR EACH ROW begin
 If new.id_resultat='' then
@@ -38,12 +19,7 @@ If new.id_resultat='' then
 	left join resultat_x_component_pro_historic as proh
 	on des.id_resultat <> proh.id_resultat);
 end if;
-end;
-|
-
-delimiter ;
-
-delimiter |
+end;|
 CREATE TRIGGER calculo_id_dest BEFORE INSERT ON resultat_x_component_des
    FOR EACH ROW begin
 If new.id_resultat='' then
@@ -56,12 +32,7 @@ If new.id_resultat='' then
 	left join resultat_x_component_pro_historic as proh
 	on des.id_resultat <> proh.id_resultat);
 end if;
-end;
-|
-
-delimiter ;
-
-delimiter |
+end;|
 CREATE TRIGGER calculo_id_pro_hist BEFORE INSERT ON resultat_x_component_pro_historic
    FOR EACH ROW begin
 If new.id_resultat='' then
@@ -74,5 +45,15 @@ If new.id_resultat='' then
 	left join resultat_x_component_pro_historic as proh
 	on des.id_resultat <> proh.id_resultat);
 end if;
-end;
-|
+end;|
+ALTER TABLE `fr_global_informe` CHANGE `id_frGlobal` `id_frGlobal`  INT NOT NULL AUTO_INCREMENT;|
+ALTER TABLE `seguim_incomp_detall` CHANGE `id_seg_inc_det` `id_seg_inc_det` INT NOT NULL AUTO_INCREMENT;|
+ALTER TABLE `seguim_incomp_resum` CHANGE `id_seg_inc_res` `id_seg_inc_res` INT NOT NULL AUTO_INCREMENT;|
+ALTER TABLE `control` CHANGE `clase_control` `clase_control_CA` VARCHAR(30) NOT NULL;|
+ALTER TABLE `control` ADD COLUMN `clase_control_ES` VARCHAR(30) NOT NULL AFTER `clase_control_CA`;|
+ALTER TABLE `excepcio` CHANGE `id_excepcio`  `id_excepcio` INT NOT NULL AUTO_INCREMENT;|
+ALTER TABLE aplicacions_obsoletes ADD COLUMN `modulo` VARCHAR(16) after `aplicacio`;|
+ALTER TABLE `germanar` CHANGE `id_germanar` `id_germanar` INT(11) NOT NULL AUTO_INCREMENT;|
+ALTER TABLE `regles_bloqueig` CHANGE `id_regla` `id_regla` INT(11) NOT NULL AUTO_INCREMENT;|
+ALTER TABLE `control` CHANGE `id_control` `id_control` INT(11) NOT NULL;|
+ALTER TABLE `control_literal` CHANGE `id_control_literal` `id_control_literal` INT(11) NOT NULL AUTO_INCREMENT;|
