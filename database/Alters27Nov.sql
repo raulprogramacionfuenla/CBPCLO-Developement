2d8cfad9a88471d4d22a39e05a67ce208918c822
Álvaro Peris <alvaroperis_06@hotmail.com>
2017-11-28 13:32:17 +0100

    Alters y formloader
    Se sube el form loader y los alters de las bases de datos de la semana del 27 de Nov

--- database/Alters27Nov.sql
+++ database/Alters27Nov.sql
@@ -3,3 +3,12 @@ drop trigger calculo_id_des_hist; 
 drop trigger calculo_id_dest; 
 drop trigger calculo_id_pro_hist; 
 drop trigger calculo_id_pro;
+
+/*Falta la descripción en catalan*/
+ALTER TABLE `tipus_component` CHANGE `descripcio` `descripcio_CA` VARCHAR(45);
+ALTER TABLE `tipus_component` ADD `descripcio_ES` VARCHAR(45) AFTER `descripcio_CA`;
+
+/*Alters de hermanamiento*/
+ALTER TABLE `germanar` ADD `dataAlta`  DATETIME
+ALTER TABLE `germanar` ADD `dataBaixa` DATETIME AFTER `dataAlta`
+ALTER TABLE `germanar` ADD `motiu` VARCHAR(200) AFTER `dataBaixa`
